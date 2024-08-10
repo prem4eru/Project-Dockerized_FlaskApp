2c38a07fda3b8026914ceccc186084a2a2910a19
prem4eru <prem4eru@gmail.com>
2024-08-10 15:15:13 +0530

    completed docker flask app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # dockerapp.py
 from flask import Flask
 
-dockerapp = Flask(__name__)
+app = Flask(__name__)
 
-@dockerapp.route('/')
+@app.route('/')
 def hello_world():
     return 'Hello, Dockerized Flask App!'
 
 if __name__ == '__main__':
-    dockerapp.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
